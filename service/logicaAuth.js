68177dbca07e514b8f708017086e7bc3f334555e
ramon <monchouam@gmail.com>
2021-12-18 19:24:29 -0500

    cambios en logicaauth para ingreso de sesion

--- service/logicaAuth.js
+++ service/logicaAuth.js
@@ -6,44 +6,51 @@ const key = "porquemambrusefuealaguerra?";
 const logicaAuth = async (datosPeticion) => {
 
     try {
-        console.log("DatosPeticion:",datosPeticion)
-        const usuario = await User.findOne({ correo: datosPeticion?.email })
-        // verificar si esta activo.
-        console.log('usuarioMOngo:',usuario)
-        if (!usuario) {   
-                return  {mensaje:"Verique usuario y contrasena",status: 401}    
+        if(datosPeticion.email == "" || datosPeticion.clave == ""){
+            return { mensaje: "Verique usuario y contrasena", status: 401 }
         }
+        if (datosPeticion.email != "" && datosPeticion.clave != "") {
 
-        console.log("claveee ", usuario.contrasena)
-        const claveDesencriptada = aes256.decrypt(key, usuario.contrasena)
-        console.log("claveee Desencriptada", claveDesencriptada)
-        console.log("datosPeticion: ", datosPeticion)
-        if (datosPeticion.clave != claveDesencriptada) {  
-            
-                return  {mensaje:"Verique usuario y contrasena",status: 401} 
+
+            console.log("DatosPeticion:", datosPeticion)
+            const usuario = await User.findOne({ correo: datosPeticion?.email })
+            // verificar si esta activo.
+            console.log('usuarioMOngo:', usuario)
+
+            if (!usuario) {
+                return { mensaje: "Verique usuario y contrasena", status: 401 }
+            }
+
+            console.log("claveee ", usuario.contrasena)
+            const claveDesencriptada = aes256.decrypt(key, usuario.contrasena)
+            console.log("claveee Desencriptada", claveDesencriptada)
+            console.log("datosPeticion: ", datosPeticion)
+            if (datosPeticion.clave != claveDesencriptada) {
+
+                return { mensaje: "Verique usuario y contrasena", status: 401 }
+            }
+
+            console.log("usuario.estado: ", usuario.estado);
+            if (usuario.estado != "AUTORIZADO") {
+
+                return { mensaje: "Pendiente de autorización o no autorizado", status: 401 }
+            }
+
+
+            const token = jwt.sign({
+
+                tipoUsuario: usuario.tipoUsuario,
+                nombre: usuario.nombre,
+                identificacion: usuario.identificacion
+
+            }, key, { expiresIn: 2000 })
+            //console.log('token::::',token)
+            return { mensaje: token, status: 200 }
+
         }
-
-        console.log("usuario.estado: ", usuario.estado);
-        if (usuario.estado != "AUTORIZADO"){  
-            
-            return  {mensaje:"Pendiente de autorización o no autorizado",status: 401} 
-        }
-
-
-        const token = jwt.sign({
-
-            tipoUsuario: usuario.tipoUsuario,
-            nombre: usuario.nombre,
-            identificacion: usuario.identificacion
-
-        }, key, { expiresIn: 2000 })
-        //console.log('token::::',token)
-        return {mensaje:token,status: 200} 
-        
-
     } catch (error) {
         //console.log(error)
-        return {mensaje:"Contante al admin",status: 500} 
+        return { mensaje: "Contante al admin", status: 500 }
     }
     // pasarnos 2 parametros. correo y contrase;a
     // debemos verfiicar el correo que exista
